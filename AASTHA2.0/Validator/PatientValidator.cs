af1aac18f797a8ed9cdf863fc01b269a691d11a9
Ginesh Tandel <ginesh29@gmail.com>
2019-11-13 13:17:38 +0530

    co

--- AASTHA2.0/Validator/PatientValidator.cs
+++ AASTHA2.0/Validator/PatientValidator.cs
@@ -7,28 +7,7 @@ namespace AASTHA2.Validator
     {
         public PatientValidator()
         {
-            RuleFor(m => m.Firstname).NotEmpty();
-            //RuleSet("all", () =>
-            //{
-            //    RuleFor(x => x.Id).Must(CheckId).WithMessage("id must greater than 0");
-            //    RuleFor(x => x.Name).NotNull().When(x => !x.Id.HasValue).WithMessage("name could not be null");
-            //});
-
-            //RuleSet("id", () =>
-            //{
-            //    RuleFor(x => x.Id).NotNull().WithMessage("id could not be null")
-            //             .GreaterThan(0).WithMessage("id must greater than 0");
-            //});
-
-            //RuleSet("name", () =>
-            //{
-            //    RuleFor(x => x.Name).NotNull().WithMessage("name could not be null");
-            //});
-        }
-
-        private bool CheckId(int? id)
-        {
-            return !id.HasValue || id.Value > 0;
+            RuleFor(m => m.Firstname).NotEmpty();            
         }
     }
 }
