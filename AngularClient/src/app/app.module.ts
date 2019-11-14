813f60f603d3e3e799a1a874f3649030231fbfed
Ginesh Tandel <ginesh29@gmail.com>
2019-11-14 17:38:45 +0530

    Angular setup with layout

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -1,13 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './account/login/login.component';
+import { LeftMenuComponent } from './shared/left-menu/left-menu.component';
+import { NotFound404Component } from './shared/not-found404/not-found404.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+    ]
+  },
+  { path: '**', component: NotFound404Component }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    DashboardComponent,
+    LoginComponent,
+    LeftMenuComponent,
+    NotFound404Component
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
