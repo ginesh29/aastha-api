c49f0fe328a0ca016384f4729decb69c5bfa0bbf
Ginesh Tandel <ginesh29@gmail.com>
2020-01-22 18:54:49 +0530

    aa

--- Repositories/OpdRepository.cs
+++ Repositories/OpdRepository.cs
@@ -12,12 +12,17 @@ namespace AASTHA2.Repositories
         {
 
         }
-        public IEnumerable<dynamic> GetStatistics(out int totalCount,string filter)
+        public IEnumerable<dynamic> GetStatistics(out int totalCount, string filter)
         {
-            var data = Find(null, out totalCount,filter)
+            var data = Find(null, out totalCount, filter)
                     .GroupBy(grp => new { Month = grp.Date.Month, Year = grp.Date.Year })
-                    .Select(g => new { Month = g.Key.Month, Year = g.Key.Year, Total = g.Count() })
-                    .OrderByDescending(a => a.Year).ThenByDescending(a => a.Month);
+                    .Select(g => new
+                    {
+                        Month = g.FirstOrDefault().Date.ToString("MMMM"),
+                        Year = g.Key.Year,
+                        TotalPatient = g.Count(),
+                        TotalCollection = g.Sum(m => m.ConsultCharge) + g.Sum(m=>m.UsgCharge) + g.Sum(m => m.UptCharge) + g.Sum(m => m.InjectionCharge) + g.Sum(m => m.OtherCharge)
+                    }).OrderByDescending(a => a.Year).ThenByDescending(a => a.Month);
             return data;
         }
     }
