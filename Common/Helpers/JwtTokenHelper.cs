90a22c2bbef39d90156ba846d8fe4b46db0aeaca
ginesh29 <ginesh29@gmail.com>
2020-12-20 00:07:49 +0530

    comment jwt

--- Common/Helpers/JwtTokenHelper.cs
+++ Common/Helpers/JwtTokenHelper.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
-using System.Linq;
+﻿//using Microsoft.AspNetCore.Http;
+//using System.Linq;
 
-namespace AASTHA2.Common.Helpers
-{
-    public class JwtTokenHelper
-    {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        public JwtTokenHelper(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
-        public string ExtractToken(string key)
-        {
-            var User = _httpContextAccessor.HttpContext.User;
-            var value = User.Claims.FirstOrDefault(claim => claim.Type == key).Value;
-            return value;
-        }
-    }
-}
+//namespace AASTHA2.Common.Helpers
+//{
+//    public class JwtTokenHelper
+//    {
+//        private readonly IHttpContextAccessor _httpContextAccessor;
+//        public JwtTokenHelper(IHttpContextAccessor httpContextAccessor)
+//        {
+//            _httpContextAccessor = httpContextAccessor;
+//        }
+//        public string ExtractToken(string key)
+//        {
+//            var User = _httpContextAccessor.HttpContext.User;
+//            var value = User.Claims.FirstOrDefault(claim => claim.Type == key).Value;
+//            return value;
+//        }
+//    }
+//}
