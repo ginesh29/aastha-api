76890aec2b6997780e6167dc4901fa2853994e83
Ginesh@LogiqCube <6223887-Ginesh.LogiqCube@users.noreply.gitlab.com>
2022-12-18 00:16:08 +0530

    namespace remove

--- AASTHA2.0/Program.cs
+++ AASTHA2.0/Program.cs
@@ -14,14 +14,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
 using System;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace AASTHA2
 {
